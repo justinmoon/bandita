5972f2bb03b08b9d8a997bd6fc60627afef86993
Justin Moon <mail@justinmoon.com>
2025-04-04 11:48:49 +0100

    Nix flake that installs golang

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A basic Go development flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+          ];
+        };
+      });
+}
